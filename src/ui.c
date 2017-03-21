2a93e21923ec4f9f1cda6140f4852e01f76ea093
Allison Knauss <piper.pandemos@gmail.com>
2017-03-21 00:11:42 -0500

    Remove dead ui code

--- src/ui.c
+++ src/ui.c
@@ -58,33 +58,6 @@ die(const char *fmt, ...)
     exit(EXIT_FAILURE);
 }
 
-static void*
-xcalloc(size_t siz, size_t n)
-{
-    void *ptr = calloc(siz, n);
-    if (!ptr) die("Out of memory\n");
-    return ptr;
-}
-
-static long
-timestamp(void)
-{
-    struct timeval tv;
-    if (gettimeofday(&tv, NULL) < 0) return 0;
-    return (long)((long)tv.tv_sec * 1000 + (long)tv.tv_usec/1000);
-}
-
-static void
-sleep_for(long t)
-{
-    struct timespec req;
-    const time_t sec = (int)(t/1000);
-    const long ms = t - (sec * 1000);
-    req.tv_sec = sec;
-    req.tv_nsec = ms * 1000000L;
-    while(-1 == nanosleep(&req, &req));
-}
-
 XWindow xw;
 struct nk_context *ctx;
 static unsigned short running = 0;
